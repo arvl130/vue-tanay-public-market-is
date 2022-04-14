e64d1154e75a97958f32169117e1d0feaabcb04e
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-14 15:50:36 +0800

    AboutPage.vue: center image and content

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -26,42 +26,46 @@ import NavBar from "../components/NavBar/NavBar.vue";
     </template>
   </NavBar>
 
-  <header class="max-w-6xl mx-auto px-6 pt-6 pb-3">
-    <h2 class="font-extrabold text-4xl mb-3">About Us</h2>
-  </header>
+  <main class="max-w-6xl mx-auto px-6 pb-12">
+    <img
+      src="../assets/img/banner.jpg"
+      class="object-cover mb-8 rounded-b-2xl"
+    />
 
-  <main class="max-w-6xl mx-auto px-6">
-    <img src="../assets/img/banner.jpg" class="object-cover mb-6" />
+    <div class="max-w-2xl mx-auto">
+      <h2 class="font-extrabold text-4xl text-center mb-4">About Us</h2>
 
-    <p class="text-justify indent-14 mb-3">
-      The <span class="font-bold">Tanay Public Market</span> (TPM) is situated
-      1.3 kilometers south of Laguna Lake in Sitio Pasipit, Barangay Plaza
-      Aldea, away from the town's municipal building. It was established in 1994
-      to replace the original public market site close to Tanay Church, which
-      was burned down the same year. The property is 3.0 hectares in size and is
-      flanked on all sides by commercial establishments. Commodities were
-      divided into sections and buildings, as follows: Dry Section - Bldgs. Wet
-      Section - Cl and C1 (part of), A, B1 & B2 & C2 (part of), to give
-      market-goers/consumers a sense of organization and direction. According to
-      data provided by the Municipal Treasury Office, the 2020 income of Tanay
-      Public Market decreased by 32.64 percent due to the suspension of payments
-      of rental fees on fixed stalls, commercial stalls, ambulant vendors,
-      terminal fees, and other fees being implemented in the public market, due
-      to the effects of the pandemic on market vendors and other users of the
-      facilities within the public market.
-    </p>
-    <p class="text-justify indent-14 mb-12">
-      The following amenities are located inside the public market area:
-      Bagsakan Area, which is located at the back of the public market and has
-      an approximate lot area of 2,200 sqm. It acts as a pick-up and drop-off
-      site for a variety of agricultural commodities supplied by local farmers
-      and retailers. Similarly, it functions differently depending on the
-      circumstances in the same area; Tanay Grand Central Terminal - terminal
-      for various public utility vehicles plying in and out of town
-      (approximately 3,800 sqm), Various Retail Stores at the - terminal,
-      Commercial Stalls / Spaces within the perimeter of the public market, BFP
-      Office / Fire Sub Station - TPM and Rest Rooms, Telephone Stations, and so
-      on.
-    </p>
+      <p class="text-justify indent-14 mb-3">
+        The <span class="font-bold">Tanay Public Market</span> (TPM) is situated
+        1.3 kilometers south of Laguna Lake in Sitio Pasipit, Barangay Plaza
+        Aldea, away from the town's municipal building. It was established in
+        1994 to replace the original public market site close to Tanay Church,
+        which was burned down the same year. The property is 3.0 hectares in
+        size and is flanked on all sides by commercial establishments.
+        Commodities were divided into sections and buildings, as follows: Dry
+        Section - Bldgs. Wet Section - Cl and C1 (part of), A, B1 & B2 & C2
+        (part of), to give market-goers/consumers a sense of organization and
+        direction. According to data provided by the Municipal Treasury Office,
+        the 2020 income of Tanay Public Market decreased by 32.64 percent due to
+        the suspension of payments of rental fees on fixed stalls, commercial
+        stalls, ambulant vendors, terminal fees, and other fees being
+        implemented in the public market, due to the effects of the pandemic on
+        market vendors and other users of the facilities within the public
+        market.
+      </p>
+      <p class="text-justify indent-14">
+        The following amenities are located inside the public market area:
+        Bagsakan Area, which is located at the back of the public market and has
+        an approximate lot area of 2,200 sqm. It acts as a pick-up and drop-off
+        site for a variety of agricultural commodities supplied by local farmers
+        and retailers. Similarly, it functions differently depending on the
+        circumstances in the same area; Tanay Grand Central Terminal - terminal
+        for various public utility vehicles plying in and out of town
+        (approximately 3,800 sqm), Various Retail Stores at the - terminal,
+        Commercial Stalls / Spaces within the perimeter of the public market,
+        BFP Office / Fire Sub Station - TPM and Rest Rooms, Telephone Stations,
+        and so on.
+      </p>
+    </div>
   </main>
 </template>
